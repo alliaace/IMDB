ae7cf17d71161be8b76dda39e2bd743100784d74
Ashar <muhammad.ashar3239@gmail.com>
2021-06-26 13:15:34 +0500

    'final'

--- src/Detail.js
+++ src/Detail.js
@@ -1,65 +1,66 @@
 import axios from 'axios'
-import React, { Component } from 'react'
-import { Image, ImageBackground, ScrollView, Text, View } from 'react-native'
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export default class Detail extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: [],
-            isVisible: false,
-        }
-    }
-    componentDidMount() {
-        axios.get(`https://www.omdbapi.com/?apikey=54816d00&i=${this.props.route.params}`)
-            .then((res) => this.setState({ data: res.data }))
+export default function Detail({ route }) {
+
+    const [data, setData] = useState([])
+    const [isVisible, setIsVisible] = useState(false)
+
+
+    useEffect(() => {
+
+        axios.get(`https://www.omdbapi.com/?apikey=54816d00&i=${route.params}`)
+            .then((res) => setData(res.data))
             .catch((err) => alert(err))
-    }
-    render() {
-        var x = this.state.data
-        return (
-            <View style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.2)", paddingHorizontal: 10 }} >
-                <Image source={{ uri: x.Poster }} style={{ height: "30%", width: "100%", marginVertical: 10 }} resizeMode="stretch" />
-                <ScrollView>
-                    <Text style={{ fontSize: 26, color: "black" }}>Title: {x.Title}</Text>
-                    <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
-                    <Text style={{ fontSize: 18 }}>Year: {x.Year}</Text>
-                    <Text style={{ fontSize: 18 }}>Genre: {x.Genre}</Text>
-                    <Text style={{ fontSize: 18 }}>Released: {x.Released}</Text>
-                    <Text style={{ fontSize: 18 }}>Runtime: {x.Runtime}</Text>
-                    <Text style={{ fontSize: 18 }}>imdbRating: {x.imdbRating}</Text>
-                    {!this.state.isVisible &&
-                        <TouchableOpacity onPress={() => this.setState({ isVisible: !this.state.isVisible })}>
-                            <Text style={{ fontSize: 18, color: "blue" }}>More...</Text>
 
-                        </TouchableOpacity>
-                    }
-                    {this.state.isVisible &&
-                        <View>
-                            <Text style={{ fontSize: 18 }}>Director: {x.Director}</Text>
-                            <Text style={{ fontSize: 18 }}>Writer: {x.Writer}</Text>
-                            <Text style={{ fontSize: 18 }}>Actor: {x.Actors}</Text>
-                            <Text style={{ fontSize: 18 }}>Language: {x.Language}</Text>
-                            <Text style={{ fontSize: 18 }}>Plot: {x.Plot}</Text>
-                            <Text style={{ fontSize: 18 }}>Country: {x.Country}</Text>
-                            <Text style={{ fontSize: 18 }}>Awards: {x.Awards}</Text>
-                            <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
-                            {x.Ratings.map((y) =>
-                                <View>
+    }, [])
 
-                                    <Text style={{ fontSize: 18 }}>Source: {y.Source}</Text>
-                                    <Text style={{ fontSize: 18 }}>Value: {y.Value}</Text>
-                                </View>
+    var x = data
+    return (
+        <View style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.2)", paddingHorizontal: 10 }} >
+            <Image source={{ uri: x.Poster }} style={{ height: "30%", width: "100%", marginVertical: 10 }} resizeMode="stretch" />
+            <ScrollView>
+                <Text style={{ fontSize: 26, color: "black" }}>Title: {x.Title}</Text>
+                <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
+                <Text style={{ fontSize: 18 }}>Year: {x.Year}</Text>
+                <Text style={{ fontSize: 18 }}>Genre: {x.Genre}</Text>
+                <Text style={{ fontSize: 18 }}>Released: {x.Released}</Text>
+                <Text style={{ fontSize: 18 }}>Runtime: {x.Runtime}</Text>
+                <Text style={{ fontSize: 18 }}>imdbRating: {x.imdbRating}</Text>
+                {!isVisible &&
+                    <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
+                        <Text style={{ fontSize: 18, color: "blue" }}>More...</Text>
 
-                            )}
-                            <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
+                    </TouchableOpacity>
+                }
+                {isVisible &&
+                    <View>
+                        <Text style={{ fontSize: 18 }}>Director: {x.Director}</Text>
+                        <Text style={{ fontSize: 18 }}>Writer: {x.Writer}</Text>
+                        <Text style={{ fontSize: 18 }}>Actor: {x.Actors}</Text>
+                        <Text style={{ fontSize: 18 }}>Language: {x.Language}</Text>
+                        <Text style={{ fontSize: 18 }}>Plot: {x.Plot}</Text>
+                        <Text style={{ fontSize: 18 }}>Country: {x.Country}</Text>
+                        <Text style={{ fontSize: 18 }}>Awards: {x.Awards}</Text>
+                        <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
+                        {x.Ratings.map((y) =>
+                            <View>
+
+                                <Text style={{ fontSize: 18 }}>Source: {y.Source}</Text>
+                                <Text style={{ fontSize: 18 }}>Value: {y.Value}</Text>
+                            </View>
+
+                        )}
+                        <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
 
 
-                        </View>
-                    }
-                </ScrollView>
-            </View>
-        )
-    }
+                    </View>
+                }
+            </ScrollView>
+        </View>
+    )
+
 }
--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,48 @@
 import axios from 'axios'
-import React, { Component } from 'react'
-import { Image, ImageBackground, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
-export default class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            movieTitle: 'forest',
-            resData: []
-        }
-    }
-    handleSearch = () => {
-        axios.get(`https://www.omdbapi.com/?s=${this.state.movieTitle}&apikey=54816d00`)
-            .then((res) => this.setState({ resData: res.data.Search }))
+export default function Home({ navigation }) {
+
+    const [movieTitle, setMovieTitle] = useState('forest')
+    const [resData, setResData] = useState([])
+    const handleSearch = () => {
+        axios.get(`https://www.omdbapi.com/?s=${movieTitle}&apikey=54816d00`)
+            .then((res) => setResData(res.data.Search))
             .catch((err) => alert(err))
     }
-    render() {
-        return (
-            <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10 }}>
-                <View style={{ flexDirection: 'row' }}>
 
-                    <TextInput placeholder="Movie Title?" style={{ width: '75%', height: 40, borderWidth: 0.5 }} onChangeText={(val) => this.setState({ movieTitle: val })} />
-                    <TouchableOpacity style={{ width: "20%", backgroundColor: "red", marginLeft: 5, justifyContent: "center", alignItems: "center" }}
-                        onPress={() => this.handleSearch()}
+    return (
+        <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center', paddingVertical: 10, paddingHorizontal: 10 }}>
+            <View style={{ flexDirection: 'row' }}>
+
+                <TextInput placeholder="Movie Title?" style={{ width: '75%', height: 40, borderWidth: 0.5 }} onChangeText={(val) => setMovieTitle(val)} />
+                <TouchableOpacity style={{ width: "20%", backgroundColor: "red", marginLeft: 5, justifyContent: "center", alignItems: "center" }}
+                    onPress={() => handleSearch()}
+                >
+                    <Text style={{ color: "white" }}>Search</Text>
+                </TouchableOpacity>
+            </View>
+            {typeof resData == 'undefined' ? <Text>No Record found</Text> : resData?.map((x) =>
+                <View style={{ height: 250, backgroundColor: "rgba(0,0,0,0.2)", width: '97%', marginTop: 10, justifyContent: "center", paddingHorizontal: 10 }}  >
+                    <TouchableOpacity style={{ flexDirection: "row", height: "100%", width: "100%", alignItems: "center", }}
+                        onPress={() => navigation.navigate("Detail", x.imdbID)}
                     >
-                        <Text style={{ color: "white" }}>Search</Text>
-                    </TouchableOpacity>
+
+                        <Image source={{ uri: x.Poster }} style={{ height: "70%", width: "30%" }} />
+                        <View style={{ marginLeft: 10, overflow: "scroll" }}>
+                            <Text style={{ fontSize: 26, fontWeight: "bold" }}>{x.Title}</Text>
+                            <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
+                            <Text style={{ fontSize: 18 }}>{x.Year}</Text>
+                            <Text style={{ fontSize: 18 }}>{x.Type}</Text>
+                        </View>
+                    </TouchableOpacity >
                 </View>
-                {typeof this.state.resData == 'undefined' ? <Text>No Record found</Text> : this.state.resData?.map((x) =>
-                    <View style={{ height: 250, backgroundColor: "rgba(0,0,0,0.2)", width: '97%', marginTop: 10, justifyContent: "center", paddingHorizontal: 10 }}  >
-                        <TouchableOpacity style={{ flexDirection: "row", height: "100%", width: "100%", alignItems: "center", }}
-                            onPress={() => this.props.navigation.navigate("Detail", x.imdbID)}
-                        >
 
-                            <Image source={{ uri: x.Poster }} style={{ height: "70%", width: "30%" }} />
-                            <View style={{ marginLeft: 10, overflow: "scroll" }}>
-                                <Text style={{ fontSize: 26, fontWeight: "bold" }}>{x.Title}</Text>
-                                <View style={{ width: "100%", height: 1, borderBottomWidth: 0.5, marginVertical: 10 }}></View>
-                                <Text style={{ fontSize: 18 }}>{x.Year}</Text>
-                                <Text style={{ fontSize: 18 }}>{x.Type}</Text>
-                            </View>
-                        </TouchableOpacity >
-                    </View>
+            )}
 
-                )}
+        </ScrollView>
+    );
 
-            </ScrollView>
-        );
-    }
 }
